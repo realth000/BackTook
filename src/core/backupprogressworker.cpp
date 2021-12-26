b1c8489b2369c31d843b163c02da282eda4f944c
realth000 <a598919335@gmail.com>
2021-12-26 22:34:30 +0800

    feat(Backup):新增BackupProgressWorker
    新增：
        新增BackupProgressWorker

---
+++ src/core/backupprogressworker.cpp
@@ -0,0 +1,30 @@
+﻿#include "backupprogressworker.h"
+
+// test
+#include <QThread>
+#include <QDebug>
+
+
+BackupProgressWorker::BackupProgressWorker(const QString &srcPath, const QString &dstPath, const CopyHelper::CopyMode &copyMode)
+    : m_srcFileInfo(srcPath),
+      m_dstFileInfo(dstPath),
+      m_copyMode(copyMode)
+{
+
+}
+
+void BackupProgressWorker::startBackup()
+{
+    if(m_srcFileInfo.isFile()){
+        // TODO: Save CopyMode in config
+        CopyHelper::copyFile(m_srcFileInfo.absoluteFilePath(), m_dstFileInfo.absoluteFilePath(), CopyHelper::CopyMode::Force);
+    }
+    else if(m_srcFileInfo.isDir()){
+        CopyHelper::copyDirectory(m_srcFileInfo.absoluteFilePath(), m_dstFileInfo.absoluteFilePath(), CopyHelper::CopyMode::Force);
+    }
+
+    // test
+    qDebug() << "worker thread id:" << QThread::currentThreadId();
+
+    emit backupFinished();
+}
---
+++ src/core/backupprogressworker.h
@@ -0,0 +1,27 @@
+﻿#ifndef BACKUPPROGRESSWORKER_H
+#define BACKUPPROGRESSWORKER_H
+
+#include <QtCore/QFileInfo>
+#include <QtCore/QObject>
+#include "utils/copyhelper.h"
+
+class BackupProgressWorker : public QObject
+{
+    Q_OBJECT
+
+public:
+    BackupProgressWorker(const QString &srcPath, const QString &dstPath, const CopyHelper::CopyMode &copyMode);
+
+signals:
+    void backupFinished();
+
+public slots:
+    void startBackup();
+
+private:
+    QFileInfo m_srcFileInfo;
+    QFileInfo m_dstFileInfo;
+    CopyHelper::CopyMode m_copyMode;
+};
+
+#endif // BACKUPPROGRESSWORKER_H
