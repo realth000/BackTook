66b6b3b118e45f27bf10fd697cec75e6995bd481
realth000 <a598919335@gmail.com>
2021-12-27 02:56:26 +0800

    docs(LICENSE):Update license in BackupProgressWorker
    Other changes:
        Update license in BackupProgressWorker.

--- src/core/backupprogressworker.cpp
+++ src/core/backupprogressworker.cpp
@@ -1,4 +1,21 @@
-﻿#include "backupprogressworker.h"
+﻿/*
+ *  Copyright (C) 2021 realth000 <a598919335@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "backupprogressworker.h"
 
 // test
 #include <QThread>
@@ -22,9 +39,5 @@ void BackupProgressWorker::startBackup()
     else if(m_srcFileInfo.isDir()){
         CopyHelper::copyDirectory(m_srcFileInfo.absoluteFilePath(), m_dstFileInfo.absoluteFilePath(), CopyHelper::CopyMode::Force);
     }
-
-    // test
-    qDebug() << "worker thread id:" << QThread::currentThreadId();
-
     emit backupFinished();
 }
